9ce996884765b57100e0a5f8d45eb24b079e9685
Stephanie Bland <Bland@Dal.ca>
2017-04-14 09:31:24 -0700

    Cleaned up Melting!

--- Rcluster.R
+++ Rcluster.R
@@ -36,59 +36,61 @@ isfish
 ################################################
 ############### Extract Data ###################
 ################################################
-B=B_test
 # Set up - helpful data reformatting:
-colnames(B)=paste0('Node_',1:nichewebsize)
 inverts_only=setdiff(which(isfish==0),basalsp) #Find invertebrates (not fish or autotrophs)
 fish_names=unique(species[isfish==1]) #The species number for fish
 yr_ls=cumsum(unlist(num.years)) # Cumulative sums of years for phases
 
 # ---- DATA_FOR_TAPPLY ----
-# 1 - add in columns for sum of nodes so we have biomass of groups of species (like all fish)
-#Fish total per species
-Fish_tot_per_sp_df=c()
-for (item in fish_names){
-  Fish_tot_per_sp_df=cbind(Fish_tot_per_sp_df,rowSums(B[,species==item]))
+lump_Bio_sums <- function(B_mat){# 1 Add columns for sum of nodes so we have biomass of groups of species (ex. all fish)
+  colnames(B_mat)=paste0('Node_',1:nichewebsize)
+  #Fish total per species
+  Fish_tot_per_sp_df=c()
+  for (item in fish_names){
+    Fish_tot_per_sp_df=cbind(Fish_tot_per_sp_df,rowSums(B_mat[,species==item]))
+  }
+  colnames(Fish_tot_per_sp_df)=paste0('Fish_sp_',fish_names)
+  
+  #Fish total per lifestage
+  Fish_tot_per_ls_df=c()
+  for (item in 1:max(N.stages)){
+    fish_stages=(lifestage==item & t(isfish)==1)
+    Fish_tot_per_ls_df=cbind(Fish_tot_per_ls_df,rowSums(B_mat[,fish_stages]))
+  }
+  colnames(Fish_tot_per_ls_df)=paste0('Fish_ls_',1:max(N.stages))
+  
+  #Find sum of basic categories
+  Tot_df=rowSums(B_mat)
+  Fish_tot_df=rowSums(B_mat[,isfish==1])#Total biomass of all fish species
+  non_fish_df=rowSums(B_mat[,isfish==0])#Total Biomass of Non Fish species
+  basal_tot_df=rowSums(B_mat[,basalsp])#Total Biomass of all autotrophs
+  inverts_tot_df=rowSums(B_mat[,inverts_only])#Total Biomass of invertebrates only
+  
+  #Bind them to regular Data frame
+  B_df=cbind(B_mat,Fish_tot_per_sp_df,Fish_tot_per_ls_df,Tot_df,Fish_tot_df,non_fish_df,basal_tot_df,inverts_tot_df)
+  # 2 Melt data so [i j B] = [day, node (or sum of nodes), Biomass]
+  first_melt=setNames(melt(B_df), c('Day_df','Nodes_df','Biomass'))
 }
-colnames(Fish_tot_per_sp_df)=paste0('Fish_sp_',fish_names)
 
-#Fish total per lifestage
-Fish_tot_per_ls_df=c()
-for (item in 1:max(N.stages)){
-  fish_stages=(lifestage==item & t(isfish)==1)
-  Fish_tot_per_ls_df=cbind(Fish_tot_per_ls_df,rowSums(B[,fish_stages]))
+melt_new_col=function(melted_df){
+  # 3 Add columns into melt for each category:
+  melted_df$Calen_df=(melted_df$Day_df-1)%%(L.year)+1
+  melted_df$Year_df=(melted_df$Day_df-1)%/%L.year+1
+  for (item in length(num.years):1){
+    melted_df$Phase_df[melted_df$Year_df <=yr_ls[item]]=item
+  }
+  melted_df$yr_in_phase=melted_df$Year_df-c(0,yr_ls)[melted_df$Phase_df]
+  melted_df[,"Seed"]=c(seed_0)
+  melted_df[,"Simnum"]=c(simnum)
+  melted_df[,"Exper"]=c(Exper)
+  result=melted_df
 }
-colnames(Fish_tot_per_ls_df)=paste0('Fish_ls_',1:max(N.stages))
 
-#Find sum of basic categories
-Tot_df=rowSums(B)
-Fish_tot_df=rowSums(B[,isfish==1])#Total biomass of all fish species
-non_fish_df=rowSums(B[,isfish==0])#Total Biomass of Non Fish species
-basal_tot_df=rowSums(B[,basalsp])#Total Biomass of all autotrophs
-inverts_tot_df=rowSums(B[,inverts_only])#Total Biomass of invertebrates only
-
-#Bind them to regular Data frame
-B_df=cbind(B,Fish_tot_per_sp_df,Fish_tot_per_ls_df,Tot_df,Fish_tot_df,non_fish_df,basal_tot_df,inverts_tot_df)
-
-# 2 Melt data so [i j B] = [day, node (or sum of nodes), Biomass]
-first_melt=setNames(melt(B_df), c('Day_df','Nodes_df','Biomass'))
-# 3 Add columns into melt for each category:
-first_melt$Calen_df=(first_melt$Day_df-1)%%(L.year)+1
-first_melt$Year_df=(first_melt$Day_df-1)%/%L.year+1
-for (item in length(num.years):1){
-  first_melt$Phase_df[first_melt$Year_df <=yr_ls[item]]=item
-}
-first_melt$yr_in_phase=first_melt$Year_df-c(0,yr_ls)[first_melt$Phase_df]
-first_melt[,"Seed"]=c(seed_0)
-first_melt[,"Simnum"]=c(simnum)
-first_melt[,"Exper"]=c(Exper)
-
-
-
-
-
-
-
+melt_B=lump_Bio_sums(B)
+melt_B.yr.end=lump_Bio_sums(B.year.end)
+melt_B.yr.end$Day_df=melt_B.yr.end$Day_df*100
+melt_B=melt_new_col(melt_B)
+melt_B.yr.end=melt_new_col(melt_B.yr.end)
 
 ################################################
 ################## Save Data ###################
@@ -96,7 +98,7 @@ first_melt[,"Exper"]=c(Exper)
 # Last line is Data entry into matrix, because line should only be entered if all data was collected
 # It loads it into line k for the data matrix. 
 c(seed_0,simnum,Exper)
-
+first_melt
 
 
 
