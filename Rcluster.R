b0deacab1df4f8e080eb0510b782c3a1e2364da4
Stephanie Bland <Bland@Dal.ca>
2017-08-25 20:16:53 -0300

    Only define functions once

--- Rcluster.R
+++ Rcluster.R
@@ -9,6 +9,53 @@
 ################################################
 ############### Temp Testing ###################
 ################################################
+#---- functions ----
+lump_Bio_sums <- function(B_mat){# 1 Add columns for sum of nodes so we have biomass of groups of species (ex. all fish)
+	colnames(B_mat)=paste0('Node_',1:nichewebsize)
+	#Fish total per species
+	Fish_tot_per_sp_df=c()
+	for (item in fish_names){
+		Fish_tot_per_sp_df=cbind(Fish_tot_per_sp_df,rowSums(B_mat[,species==item]))
+	}
+	colnames(Fish_tot_per_sp_df)=paste0('Fish_sp_',1:length(fish_names))
+	
+	#Fish total per lifestage
+	Fish_tot_per_ls_df=c()
+	for (item in 1:max(N_stages)){
+		fish_stages=(lifestage==item & t(isfish)==1)
+		Fish_tot_per_ls_df=cbind(Fish_tot_per_ls_df,rowSums(B_mat[,fish_stages]))
+	}
+	colnames(Fish_tot_per_ls_df)=paste0('Fish_ls_',1:max(N_stages))
+	
+	#Find sum of basic categories
+	Tot_df=rowSums(B_mat)
+	Fish_tot_df=rowSums(B_mat[,isfish==1])#Total biomass of all fish species
+	non_fish_df=rowSums(B_mat[,isfish==0])#Total Biomass of Non Fish species
+	basal_tot_df=rowSums(as.matrix(B_mat[,basalsp]))#Total Biomass of all autotrophs
+	inverts_tot_df=rowSums(as.matrix(B_mat[,inverts_only]))#Total Biomass of invertebrates only
+	
+	#Bind them to regular Data frame
+	B_df=cbind(B_mat,Fish_tot_per_sp_df,Fish_tot_per_ls_df,Tot_df,Fish_tot_df,non_fish_df,basal_tot_df,inverts_tot_df)
+	B_df=as.matrix(B_df)
+}
+
+melt_new_col=function(melted_df){
+	# 3 Add columns into melt for each category:
+	melted_df$Calen_df=(melted_df$Day_df-1)%%(L_year)+1
+	melted_df$Year_df=(melted_df$Day_df-1)%/%L_year+1
+	for (item in length(numyears):1){
+		melted_df$Phase_df[melted_df$Year_df <=yr_ls[item]]=item
+	}
+	melted_df$yr_in_phase=melted_df$Year_df-c(0,yr_ls)[melted_df$Phase_df]
+	melted_df[,"Seed"]=c(seed_0)
+	melted_df[,"Simnum"]=c(simnum)
+	melted_df[,"Exper"]=c(Exper)
+	melted_df[,"Prey"]=c(prey)
+	melted_df[,"Pred"]=c(pred)
+	melted_df[,"ShouldBe0"]=c(abs(prey-lifehis.splitdiet)+abs(pred-lifehis.fishpred))
+	result=melted_df
+}
+
 #---- Hidden ----
 rm(list=ls())
 library(reshape2)
@@ -55,52 +102,6 @@ for (simnum in c(1:400)){
     colnames(Timegroups)=c("Year_start","Year_end","Year_length","Day_start","Day_end","Days_length")
     
     # ---- DATA_FOR_TAPPLY ----
-    lump_Bio_sums <- function(B_mat){# 1 Add columns for sum of nodes so we have biomass of groups of species (ex. all fish)
-      colnames(B_mat)=paste0('Node_',1:nichewebsize)
-      #Fish total per species
-      Fish_tot_per_sp_df=c()
-      for (item in fish_names){
-        Fish_tot_per_sp_df=cbind(Fish_tot_per_sp_df,rowSums(B_mat[,species==item]))
-      }
-      colnames(Fish_tot_per_sp_df)=paste0('Fish_sp_',1:length(fish_names))
-      
-      #Fish total per lifestage
-      Fish_tot_per_ls_df=c()
-      for (item in 1:max(N_stages)){
-        fish_stages=(lifestage==item & t(isfish)==1)
-        Fish_tot_per_ls_df=cbind(Fish_tot_per_ls_df,rowSums(B_mat[,fish_stages]))
-      }
-      colnames(Fish_tot_per_ls_df)=paste0('Fish_ls_',1:max(N_stages))
-      
-      #Find sum of basic categories
-      Tot_df=rowSums(B_mat)
-      Fish_tot_df=rowSums(B_mat[,isfish==1])#Total biomass of all fish species
-      non_fish_df=rowSums(B_mat[,isfish==0])#Total Biomass of Non Fish species
-      basal_tot_df=rowSums(as.matrix(B_mat[,basalsp]))#Total Biomass of all autotrophs
-      inverts_tot_df=rowSums(as.matrix(B_mat[,inverts_only]))#Total Biomass of invertebrates only
-      
-      #Bind them to regular Data frame
-      B_df=cbind(B_mat,Fish_tot_per_sp_df,Fish_tot_per_ls_df,Tot_df,Fish_tot_df,non_fish_df,basal_tot_df,inverts_tot_df)
-      B_df=as.matrix(B_df)
-    }
-    
-    melt_new_col=function(melted_df){
-      # 3 Add columns into melt for each category:
-      melted_df$Calen_df=(melted_df$Day_df-1)%%(L_year)+1
-      melted_df$Year_df=(melted_df$Day_df-1)%/%L_year+1
-      for (item in length(numyears):1){
-        melted_df$Phase_df[melted_df$Year_df <=yr_ls[item]]=item
-      }
-      melted_df$yr_in_phase=melted_df$Year_df-c(0,yr_ls)[melted_df$Phase_df]
-      melted_df[,"Seed"]=c(seed_0)
-      melted_df[,"Simnum"]=c(simnum)
-      melted_df[,"Exper"]=c(Exper)
-      melted_df[,"Prey"]=c(prey)
-      melted_df[,"Pred"]=c(pred)
-      melted_df[,"ShouldBe0"]=c(abs(prey-lifehis.splitdiet)+abs(pred-lifehis.fishpred))
-      result=melted_df
-    }
-    
     #B_df=lump_Bio_sums(B)
     B_df_yr_ends=lump_Bio_sums(B_year_end)
     # 2 Melt data so [i j B] = [day, node (or sum of nodes), Biomass]
